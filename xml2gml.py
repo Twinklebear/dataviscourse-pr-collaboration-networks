e927854e2de148826484c0e8b39fbaf8cf34850a
Kevin Wall <kwall@sci.utah.edu>
2015-11-11 13:14:31 -0700

    Added formal command line options system. Adde ability to filter results based on journal tag. Part of this solution was reowrking the edge format. Now edges also have a property that points to the article data. We may want to make this instead point to an article object, but we would need to make article objects have journal references

--- xml2gml.py
+++ xml2gml.py
@@ -8,15 +8,7 @@ import hashlib
 import json
 import os
 import re
-
-# try:
-#       from bs4 import BeautifulSoup
-#     except ImportError:
-#       try:
-#         from BeautifulSoup import BeautifulSoup
-#       except ImportError:
-#         print "BeautifulSoup not present."
-#         sys.exit()
+import getopt
 
 nodes = {}
 edges = []
@@ -42,15 +34,39 @@ def saveEdges2Json():
         json.dump(edges, f)
     return True;
 
-def save2GML(make_collaboration_graph= False):
+def edgeTest(edge, make_collaboration_graph, journal_tags):
+    node_a_hash= edge[0][0]
+    node_b_hash= edge[0][1]
+    article_data= edge[1]
+    
+    if make_collaboration_graph:
+        if nodes[node_a_hash].type!= 1 or nodes[node_b_hash].type!= 1:
+            return False
+    else:
+        if nodes[node_a_hash].type== 1 and nodes[node_b_hash].type== 1:
+            return False
+          
+    if article_data.journaltag not in journal_tags:
+      return False
+    
+    return True
+
+def save2GML(make_collaboration_graph= False, journal_tags= None):
     f = open(GML, 'w')
     f.write("graph" + '\n')
     f.write("[" + '\n')
-    for n in nodes:
-        n = nodes[n]
-        if make_collaboration_graph and n.type!= 1:
+    
+    connected_node_hashes= set()
+    for e in edges:
+        if not edgeTest(e, make_collaboration_graph, journal_tags):
             continue
-        
+        connected_node_hashes.add(e[0][0])
+        connected_node_hashes.add(e[0][1])
+    
+    for node_hash in nodes:
+        n = nodes[node_hash]
+        if node_hash not in connected_node_hashes:
+          continue
         
         f.write('  node' + '\n')
         f.write('  [' + '\n')
@@ -68,19 +84,20 @@ def save2GML(make_collaboration_graph= False):
             f.write('    type '+str(n.type) + '\n')
             f.write('    title \"'+str(n.title.encode('ascii',errors='ignore')).replace("\"", "\'") + '\"\n')
         f.write('  ]' + '\n')
+    
     for e in edges:
-        if make_collaboration_graph:
-            if nodes[e[0]].type!= 1 or nodes[e[1]].type!= 1:
-                continue
-        else:
-            if nodes[e[0]].type== 1 and nodes[e[1]].type== 1:
-                continue
+        if not edgeTest(e, make_collaboration_graph, journal_tags):
+            continue
+      
+        node_a_hash= e[0][0]
+        node_b_hash= e[0][1]
         
         f.write('  edge' + '\n')
         f.write('  [' + '\n')
-        f.write('    source ' + str(e[0]) + '\n')
-        f.write('    target ' +str(e[1]) + '\n')
+        f.write('    source ' + str(node_a_hash) + '\n')
+        f.write('    target ' +str(node_b_hash) + '\n')
         f.write('  ]' + '\n')
+    
     f.write("]")
     f.close()
 # def saveNodes2GML():
@@ -137,11 +154,13 @@ class Journal():
         self.type = 3
         self.id = ""
         self.title = ""
+        self.journaltag= ""
         
-    def __init__(self, title):
+    def __init__(self, title, journaltag):
         self.type = 3
         self.id = hashString(title)
         self.title = title
+        self.journaltag= journaltag
 
     def setTitle(self,title):
         self.title = title;
@@ -191,13 +210,6 @@ class JournalXML2GML():
                     nodes[authorkey] = Author(author)
                 edges.append([authorkey, hashkey_journal])
                 edges.append([authorkey, hashkey_title])
-                
-                for collaborator in self.authors:
-                    if collaborator is author:
-                        continue
-                
-                    collaboratorkey= hashString(collaborator)
-                    edges.append([authorkey, collaboratorkey])
                   
                 
             if hashkey_title not in nodes:
@@ -235,29 +247,33 @@ def XMLParser(filename):
             data.url = article.getElementsByTagName('ee')[0].childNodes[0].data
         data.mdate = article.getAttribute('mdate')
         data.key = article.getAttribute('key')
+        
+        journal_tag_match= re.match("journals/([^/]*)/.*", data.key)
+        data.journaltag= journal_tag_match.group(1) if journal_tag_match is not None else None
+        
         hashkey_title = hashString(data.title);
         hashkey_journal = hashString(data.journal);
-        edges.append([hashkey_title, hashkey_journal])
+        edges.append(((hashkey_title, hashkey_journal), data))
         hashkey_authors = [];
         for author in data.authors:
             authorkey = hashString(author)
             hashkey_authors.append(authorkey)
             if authorkey not in nodes:
                 nodes[authorkey] = Author(author)
-            edges.append([authorkey, hashkey_journal])
-            edges.append([authorkey, hashkey_title])
+            edges.append(((authorkey, hashkey_journal), data))
+            edges.append(((authorkey, hashkey_title), data))
             
             for collaborator in data.authors:
                 if collaborator is author:
                     continue
             
                 collaboratorkey= hashString(collaborator)
-                edges.append([authorkey, collaboratorkey])
+                edges.append(((authorkey, collaboratorkey), data))
             
         if hashkey_title not in nodes:
             nodes[hashkey_title] = Article(data.title, data.year, data.url)
         if hashkey_journal not in nodes:
-            nodes[hashkey_journal] = Journal(data.journal)
+            nodes[hashkey_journal] = Journal(data.journal, data.journaltag)
 
 def cleanupTxtFile(filename):
     # Read in the file
@@ -274,15 +290,33 @@ def cleanupTxtFile(filename):
 
 if __name__ == "__main__":
     print "XML to GML "
-    readfile = sys.argv[1]
     
-    #we may consider adding a formal option functionality in the future
-    make_collaboration_graph= False
-    if(len(sys.argv) > 2 and sys.argv[2]== "-collaboration"):
-        make_collaboration_graph= True
-      
+    options= None
+    arguments= None
+    try:
+        options, arguments= getopt.getopt(sys.argv[1:], "", ["collaboration", "journal-tags="])
+    except getopt.GetoptError as error:
+        print "options entered are invalid"
+        sys.exit()
+    if len(arguments)== 0:
+        print "didn't specify xml file"
+        sys.exit()
+        
+    readfile = arguments[0]
     if os.path.exists(readfile) is False:
         print "File doesn't exist."
+    
+    make_collaboration_graph= False
+    journal_tags= None
+    
+    for option, argument in options:
+        if option== "--collaboration":
+            make_collaboration_graph= True
+        elif option== "--journal-tags":
+            journal_tags= []
+            for journal_tag in argument.split(","):
+                journal_tags.append(journal_tag)
+    
     cleanupTxtFile(readfile)
     # xml2gml(readfile, savefile)
     # create an XMLReader
@@ -298,4 +332,4 @@ if __name__ == "__main__":
     # saveNodes2Json()
     # saveEdges2Json()
     print "done"
-    save2GML(make_collaboration_graph)
+    save2GML(make_collaboration_graph, journal_tags)
