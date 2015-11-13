239f87ed620ac0aa4e23a8bce8cbb80cb2fa0def
Will Usher <willusher.life@gmail.com>
2015-11-12 19:04:54 -0700

    Now save journal and author data out to JSON files
    The journal also includes the list of articles in it. Authors are
    identified by a unique int and author info is stored under
    ./data/authors/id

--- dblp_to_json.py
+++ dblp_to_json.py
@@ -2,8 +2,13 @@
 
 import sys
 import json
+import os
 import re
 import xml.etree.cElementTree as cET
+
+class DBLPEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.to_dict()
 
 class Author():
     def __init__(self, name, id):
@@ -16,6 +21,23 @@ class Author():
             "id": self.id
         }
 
+class Article():
+    def __init__(self, id):
+        self.title = ""
+        self.doi = ""
+        self.authors = []
+        self.year = -1
+        self.id = id
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "doi": self.doi,
+            "authors": self.authors,
+            "year": self.year,
+            "id": self.id
+        }
+
 class Journal():
     def __init__(self):
         self.title = ""
@@ -24,6 +46,7 @@ class Journal():
         self.first_year = 9999
         self.latest_year = 0
         self.authors = []
+        self.articles = []
 
     def __init__(self, title, short_name):
         self.title = title
@@ -32,6 +55,7 @@ class Journal():
         self.first_year = 9999
         self.latest_year = 0
         self.authors = []
+        self.articles = []
 
     # Format the journal information to a dict so we can dump it to JSON
     def to_dict(self):
@@ -42,8 +66,16 @@ class Journal():
             "num_articles": self.num_articles,
             "first_year": self.first_year,
             "latest_year": self.latest_year,
-            "authors": self.authors
+            "authors": self.authors,
+            "articles": self.articles,
         }
+
+# Check that the data and data/authors directories exist. If not, create them
+def check_directories():
+    if not os.path.isdir("./data/"):
+        os.mkdir("./data")
+    if not os.path.isdir("./data/authors/"):
+        os.mkdir("./data/authors")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -51,6 +83,8 @@ if __name__ == "__main__":
         print("\tThe journal filters JSON file specifies the short names of journals that")
         print("\tyou're interested in getting the reduced JSON data for")
         sys.exit(1)
+
+    check_directories()
 
     desired_journals = {}
     with open(sys.argv[2], "r") as f:
@@ -60,11 +94,13 @@ if __name__ == "__main__":
     match_journal_key = re.compile("journals\/(\w+)\/")
 
     journal = None
-    # TODO: We will want to keep the author information across journals, otherwise we would lose
+    # We want to keep the author information across journals, otherwise we would lose
     # information that one author published in many journals. Instead we would see them as
     # multiple authors, one for each journal they published in.
     authors = {}
     next_author_id = 0
+    # We also keep a single article id counter
+    next_article_id = 0
 
     context = iter(cET.iterparse(sys.argv[1], events=("start", "end")))
     event, root = next(context)
@@ -87,7 +123,8 @@ if __name__ == "__main__":
                         if journal:
                             print("Saving out {}\n\tnum_authors = {}\n\tnum_articles = {}".format(journal.title,
                                 len(journal.authors), journal.num_articles))
-                            print(json.dumps(journal.to_dict()))
+                            with open("./data/" + journal.short_name + ".json", "w") as fp:
+                                json.dump(journal, fp, cls=DBLPEncoder, indent=4)
                         # Setup the new journal we're reading
                         title = ""
                         # Find the journal's full title
@@ -96,8 +133,14 @@ if __name__ == "__main__":
                         journal = Journal(title, key.group(1))
                     # Add this article to the journal
                     journal.num_articles += 1
+
+                    # Parse the article information
+                    article = Article(next_article_id)
+                    next_article_id += 1
                     for child in list(elem):
-                        if child.tag == "author":
+                        if child.tag == "title":
+                            article.title = child.text
+                        elif child.tag == "author":
                             # This is the first time we've seen this author, so we need to add a new Author entry
                             auth = None
                             if not child.text in authors:
@@ -106,13 +149,19 @@ if __name__ == "__main__":
                                 next_author_id += 1
                             else:
                                 auth = authors[child.text]
+                            article.authors.append(auth.id)
                             # If this author isn't already recorded for this journal, add their id
                             if not auth.id in journal.authors:
                                 journal.authors.append(auth.id)
-
                         elif child.tag == "year":
-                            journal.first_year = min(journal.first_year, int(child.text))
-                            journal.latest_year = max(journal.latest_year, int(child.text))
+                            year = int(child.text)
+                            journal.first_year = min(journal.first_year, year)
+                            journal.latest_year = max(journal.latest_year, year)
+                            article.year = year
+                        elif child.tag == "ee":
+                            article.doi = child.text
+
+                    journal.articles.append(article)
             # Dump the node we just parsed and any references to it so we don't explode our memory
             root.clear()
 
@@ -120,7 +169,13 @@ if __name__ == "__main__":
     if journal:
         print("Saving out {}\n\tnum_authors = {}\n\tnum_articles = {}".format(journal.title,
             len(journal.authors), journal.num_articles))
-        print(json.dumps(journal.to_dict()))
+        with open("./data/" + journal.short_name + ".json", "w") as fp:
+            json.dump(journal, fp, cls=DBLPEncoder, indent=4)
 
-    # TODO: Save out the author information we've read in
+    # Save out the author information we've read in
+    print("Saving out {} authors".format(len(authors)))
+    for _, author in authors.items():
+        with open("./data/authors/" + str(author.id) + ".json", "w") as fp:
+            json.dump(author, fp, cls=DBLPEncoder, indent=4)
+
 
