1add26e6885cc9007ade0fe45df0cca93c9c26e0
Will Usher <willusher.life@gmail.com>
2015-12-03 13:30:07 -0700

    Add index view in side bar, also handle year filtering events
    When filtering by a year range with the brush, just show the articles
    for the author or journal that are in the range.

--- js/selection_detail.js
+++ js/selection_detail.js
@@ -1,14 +1,22 @@
 // SelectionDetail keeps the selection detail info panel up to date
 // use select_journal and select_author to update the display
 // with what the user has currently selected.
-// TODO: This should just take an event handler
 var SelectionDetail = function(event_handler) {
+	this.journal_index_div = d3.select("#journal_index");
 	this.author_info_div = d3.select("#author_info");
 	this.journal_info_div = d3.select("#journal_info");
 
+	// Current journal or author being shown
+	this.author = null;
+	this.journal = null;
+
+	this.authors = null;
+
+	// Template for information about an article in the journal or author
+	// we're showing
 	this.article_html =
 		'<div class="panel-heading">' +
-			'<h3 id="title" class="panel-title">Article Title</h3>' +
+			'<h3 id="title" class="panel-title"></h3>' +
 		'</div>' +
 		'<div class="panel-body">' +
 			'<label for="authors">Authors:</label>' +
@@ -16,6 +24,20 @@ var SelectionDetail = function(event_handler) {
 			'<label for="year">Year Published:</label>' +
 			'<p id="year"></p>' +
 			'<a id="doi" href=""></a>' +
+		'</div>';
+
+	// Template for information about a journal in the index
+	this.journal_index_html =
+		'<div class="panel-heading">' +
+			'<h3 id="title" class="panel-title"></h3>' +
+		'</div>' +
+		'<div class="panel-body">' +
+			'<label for="authors">Number of Authors:</label>' +
+			'<p id="authors"></p>' +
+			'<label for="articles">Number of Articles:</label>' +
+			'<p id="articles"></p>' +
+			'<label for="journal_years">Years in Database:</label>' +
+			'<p id="journal_years"></p>' +
 		'</div>';
 
 	// Setup ourself to listen for events we want
@@ -36,15 +58,41 @@ var SelectionDetail = function(event_handler) {
 }
 SelectionDetail.prototype.index_loaded = function(index, authors) {
 	this.authors = authors;
-	console.log("will show index summary");
-	console.log(index);
+	this.show_index(index);
+}
+SelectionDetail.prototype.show_index = function(index) {
+	// Hide the author and journal info divs
+	this.author_info_div.style("display", "none");
+	this.journal_info_div.style("display", "none");
+	this.journal_index_div.style("display", "");
+
+	// Select and update the journal info panels
+	var journals = this.journal_index_div.select("#journal_index_list");
+	var journal_list = journals.selectAll(".panel")
+		.data(Object.keys(index));
+	journal_list.exit().remove();
+	journal_list.enter().append("div")
+		.attr("class", "panel panel-default")
+		.html(this.journal_index_html);
+	var self = this;
+	journal_list.each(function(d) { self.update_journal_index(d3.select(this), index[d]); });
+}
+SelectionDetail.prototype.update_journal_index = function(element, journal) {
+	element.select("#title").html(journal.title);
+	element.select("#authors").text(journal.num_authors);
+	element.select("#articles").text(journal.num_articles);
+	element.select("#journal_years").html(journal.first_year + " &ndash; " + journal.latest_year);
 }
 // Update the selection detail panel to display information about the
 // journal selected by the user
-SelectionDetail.prototype.select_journal = function(journal) {
-	this.journal = journal;
-	this.journal_info_div.style("display", "");
+SelectionDetail.prototype.select_journal = function(journal, article_filter) {
 	this.author_info_div.style("display", "none");
+	this.journal_index_div.style("display", "none");
+	this.journal_info_div.style("display", "");
+
+	// Toggle that we're in the journal view and save the journal
+	this.journal = journal;
+	this.author = null;
 
 	this.journal_info_div.select("#journal_title").text(journal.title);
 	this.journal_info_div.select("#journal_article_count").text(journal.num_articles);
@@ -52,8 +100,12 @@ SelectionDetail.prototype.select_journal = function(journal) {
 
 	// Select and update the journals's article panels, adding/removing as needed
 	var publications = this.journal_info_div.select("#journal_papers_list");
+	var articles = journal.articles;
+	if (article_filter !== undefined){
+		articles = articles.filter(article_filter);
+	}
 	var article_list = publications.selectAll(".panel")
-		.data(journal.articles);
+		.data(articles);
 	article_list.exit().remove();
 	article_list.enter().append("div")
 		.attr("class", "panel panel-default")
@@ -66,9 +118,15 @@ SelectionDetail.prototype.author_selected = function(author_id) {
 }
 // Update the selection detail panel to display information about the
 // author currently selected by the user.
-SelectionDetail.prototype.select_author = function(author) {
+SelectionDetail.prototype.select_author = function(author, article_filter) {
 	this.journal_info_div.style("display", "none");
+	this.journal_index_div.style("display", "none");
 	this.author_info_div.style("display", "");
+
+	// Toggle that we're in the author view and save the author
+	this.journal = null;
+	this.author = author;
+
 	this.author_info_div.select("#author_name").text(author.name);
 	this.author_info_div.select("#author_affiliation").text(author.affiliation);
 
@@ -79,8 +137,12 @@ SelectionDetail.prototype.select_author = function(author) {
 
 	// Select and update the author's article panels, adding/removing as needed
 	var publications = this.author_info_div.select("#author_papers_list");
+	var articles = author.articles;
+	if (article_filter !== undefined){
+		articles = articles.filter(article_filter);
+	}
 	var article_list = publications.selectAll(".panel")
-		.data(author.articles);
+		.data(articles);
 	article_list.exit().remove();
 	article_list.enter().append("div")
 		.attr("class", "panel panel-default")
@@ -109,6 +171,16 @@ SelectionDetail.prototype.update_article = function(element, article) {
 		.attr("href", article.doi);
 }
 SelectionDetail.prototype.select_years = function(start, end) {
-	console.log("TODO: WILL selection detail select years");
+	// If we're in the journal view we want to filter the journal's articles
+	// otherwise if we're in the author view filter the author's articles
+	if (this.journal !== null){
+		this.select_journal(this.journal, function(d) {
+			return d.year >= start && d.year <= end;
+		});
+	} else if (this.author !== null){
+		this.select_author(this.author, function(d) {
+			return d.year >= start && d.year <= end;
+		});
+	}
 }
 
