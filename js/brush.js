ea5505a540468766173d0cd032f75ad1b25ce98f
Will Usher <willusher.life@gmail.com>
2015-12-03 17:48:09 -0700

    Functional brushing and update for authors now

--- js/brush.js
+++ js/brush.js
@@ -18,24 +18,28 @@ var BrushView = function(event_handler) {
     this.y_scale = d3.scale.linear().range([this.height, 0]);
 
     this.x_axis = d3.svg.axis().scale(this.x_scale).orient("bottom");
-    this.y_axis = d3.svg.axis().scale(this.y_scale).orient("left");
+    this.y_axis = d3.svg.axis().scale(this.y_scale).orient("left")
+		.tickFormat(d3.format("d"));
 
-	this.bar_group = this.svg.append("g")
+	this.group = this.svg.append("g")
 		.attr("transform", "translate(" + this.margin.left + ", " + this.margin.top + ")");
-	this.bar_group.append("g").attr("class", "axis x_axis")
+	this.group.append("g").attr("class", "axis x_axis")
 		.attr("transform", "translate(0, " + this.height + ")");
-	this.bar_group.append("g").attr("class", "axis y_axis");
+	this.group.append("g").attr("class", "axis y_axis");
+	this.bar_group = this.group.append("g");
 
     this.brush = d3.svg.brush().x(this.x_scale)
 		.on("brushend", function(){ self.select_brushed(); });
 
-	this.bar_group.append("g")
+	this.group.append("g")
 		.attr("class", "brush")
 		.attr("transform", "translate(-3, 0)");
 
-	// TODO: Emit a brush changed event when the brush is changed
 	this.event_handler = event_handler;
 	// Setup ourself to listen for events we want
+	event_handler.on("index_loaded.brush_view", function(index, authors) {
+		self.authors = authors;
+	});
 	event_handler.on("journal_selected.brush_view", function(journal, clusters, stats) {
 		self.select_journal(stats);
 	});
@@ -47,31 +51,56 @@ var BrushView = function(event_handler) {
 BrushView.prototype.select_journal = function(data) {
 	var self = this;
 
+	// Convert type of the CSV data to ints
 	data.forEach(function (d) {
         d.count = +d.count;
-        d.year = d3.time.format("%Y").parse(d.year).getFullYear();
+        d.year = +d.year;
     });
-
-	this.x_scale.domain(data.map(function(d) { return d.year; }));
+	this.draw_histogram(data);
+}
+BrushView.prototype.author_selected = function(author_id) {
+	var author = this.authors[author_id];
+	// Build the article counts for each year from this author's articles
+	var data = {};
+	for (var i = 0; i < author.articles.length; ++i){
+		var a = author.articles[i];
+		if (!data[a.year]){
+			data[a.year] = 1;
+		} else {
+			data[a.year] += 1;
+		}
+	}
+	console.log(data);
+	// Flatten to an array that draw_histogram expects
+	data = Object.keys(data).map(function(k) {
+		return { year: +k, count: data[k] };
+	});
+	this.draw_histogram(data);
+}
+BrushView.prototype.draw_histogram = function(data) {
+	var self = this;
+	this.x_scale.domain(d3.range(1960, 2016, 1));
 	this.y_scale.domain([0, d3.max(data.map(function(d) { return d.count; }))]);
 	this.y_axis.scale(this.y_scale);
 
-	this.bar_group.select(".x_axis").call(this.x_axis)
+	// Setup the x-axis labels
+	this.group.select(".x_axis").call(this.x_axis)
 		.selectAll("text")
         .attr("transform", "rotate(90)")
 		.attr("x", 20)
 		.attr("y", -8);
-	this.bar_group.select(".y_axis").call(this.y_axis);
+	this.group.select(".y_axis").call(this.y_axis);
 
+	// Draw our bars
 	var bars = this.bar_group.selectAll("rect")
 		.data(data);
 	bars.exit().remove();
 	bars.enter().append("rect").attr("class", "bar")
-		.attr("x", function(d) {
+		.attr("width", this.x_scale.rangeBand());
+	bars.attr("x", function(d) {
 			return self.x_scale(d.year);
 		})
-		.attr("width", this.x_scale.rangeBand());
-	bars.attr("y", function(d) {
+		.attr("y", function(d) {
 			return self.y_scale(d.count);
 		})
 		.attr("height", function(d) {
@@ -83,13 +112,7 @@ BrushView.prototype.select_journal = function(data) {
 		.selectAll("rect")
 		.attr("height", this.height);
 }
-BrushView.prototype.author_selected = function(author) {
-	// TODO: Show histogram of author's publication
-	console.log("TODO: WILL BrushView author selected");
-	this.clear_brush();
-}
 BrushView.prototype.select_brushed = function() {
-	console.log("TODO: WILL BrushView select brushed");
 	var scale = d3.scale.linear().domain(this.x_scale.domain()).range(this.x_scale.range());
 	var extent = this.brush.extent();
 	console.log("extent = " + extent);
@@ -103,7 +126,7 @@ BrushView.prototype.select_brushed = function() {
 	console.log("start = " + start + ", end = " + end);
 	// Snap the rect edges
 	d3.select(".brush").transition().call(this.brush.extent([scale(start), scale(end)]));
-	this.event_handler.brush_changed(start, end);
+	this.event_handler.brush_changed(start, end - 1);
 }
 BrushView.prototype.clear_brush = function() {
 	d3.select(".brush").call(this.brush.clear());
